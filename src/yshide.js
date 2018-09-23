ea166006eb6aad5539af9bcd6809652daa7287c0
Oleg Toronov <oleg.toronov@gmail.com>
2018-09-23 14:15:37 +0300

    *native js

--- src/yshide.js
+++ src/yshide.js
@@ -2,17 +2,31 @@
  * raidnew@gmail.com
  */
 
-function hideSovetnik(){
-    var node = jQuery("div").filter(function(){ return jQuery(this).css('background-color') == "rgb(250, 223, 118)" || jQuery(this).css('background-color') == "#fadf76";})[0];
-    while(node.parentNode.nodeName.toLowerCase() != "body"){
-        node = node.parentNode;
-    }
-    jQuery(node).attr("style","display:none !important;");
-    jQuery("html").attr("style","margin-top: 0");
-}
+(function() {
+    function hideSovetnik(){
 
-jQuery(document).ready(function(){
-    document.body.addEventListener('DOMSubtreeModified', function () {
+        var allDivs = document.getElementsByTagName("div");
+        var count = 0;
+        var found = false;
+        for(i = 0;i < allDivs.length; i++)
+        {
+            if (window.getComputedStyle(allDivs[i]).getPropertyValue("background-color") == "rgb(250, 223, 118)") {
+                var node = allDivs[i];
+                var html = document.getElementsByTagName("html");
+                while(node.parentNode.nodeName.toLowerCase() != "body" && count < 20){
+                    node = node.parentNode;
+                    count++;
+                }
+                node.setAttribute("style","display:none !important;");
+                html[0].setAttribute("style","margin-top: 0");
+            }
+        }
+    }
+
+    document.addEventListener("DOMContentLoaded", function(event) {
+        document.body.addEventListener('DOMSubtreeModified', function () {
+            hideSovetnik();
+        }, false);
         hideSovetnik();
-    }, false);
-});
+    });
+})();
